789a5c0480ed1765c4f5d22f2797fc8d907b69b7
Jonathan Havlicek <jhavlicek91@gmail.com>
2013-03-27 00:02:13 -0400

    Added more functioanlity to the interface

--- ui.py
+++ ui.py
@@ -2,28 +2,11 @@ from Tkinter import *
 import urllib
 import xlwt
 import os
+import re
 
+#Make window
 master = Tk()
-
 master.title("Project2")
-
-def text():
-    type = "HTML"
-
-def text2():
-    type = "PDF"
-
-def usertxt():
-    type = "TXT"
-
-def Go():
-    searchfile = fileEnter.get()
-    keywords =  keywordEnter.get()
-    excelfile = outputEnter.get()
-
-    os.system("python pdf2txt.py -o temp.txt " + searchfile + ".pdf") 
-    print "a"
-
 
 #Add file heading
 head = Label(master, text="File:")
@@ -33,16 +16,17 @@ head.grid(row=0,column=0,sticky= W)
 fileEnter = Entry(master)
 fileEnter.grid(row=1,column=0)
 
+ftype = IntVar() 
 #Add HTML Radio Button
-HTMLbutton = Radiobutton(master, text="HTML", fg="black", command=text,variable=type, value =1)
+HTMLbutton = Radiobutton(master, text="HTML", fg="black", variable = ftype, value =1)
 HTMLbutton.grid(row=2,column=0,sticky=E)
 
 #Add PDF Radio Button
-PDFbutton = Radiobutton(master, text="PDF ", fg="black", command=text2, variable= type, value =2 )
+PDFbutton = Radiobutton(master, text="PDF ", fg="black", variable= ftype, value =2 )
 PDFbutton.grid(row=2, column=2)
 
 #Add text Radio button
-TXTbutton = Radiobutton(master, text="TXT", fg="black", command= usertxt,variable=type, value= 3)
+TXTbutton = Radiobutton(master, text="TXT", fg="black", variable= ftype, value= 3)
 TXTbutton.grid(row=2,column=3)
 
 #Add keyword heading 
@@ -61,8 +45,57 @@ head2.grid(row=6,column=0,sticky=W)
 outputEnter = Entry(master)
 outputEnter.grid(row=7,column=0)
 
+def ReadTextFile(FileName, keywords):
+    #Get the text file to search through and open it
+    f = file(FileName, 'r')
+
+    #Get the keyword(s) you want to search for
+    words = keywords.split()
+
+    #Create dictionary that has each keyword and the 
+    #number of times it appeared in the file
+    new_dict = {}
+
+    for w in words:
+       new_dict[w] = 0
+    
+    for line in f:
+        for w in words:
+            instances = re.findall(w, line)
+            amount = len(instances)
+            new_amount = amount + new_dict[w]
+            new_dict[w] = new_amount
+            
+    for key in new_dict.keys():
+        print "%r: %r" % (key, new_dict[key])
+
+    #Close the file you read
+    f.close()
+
+#Function for when go is clicked
+def Go():
+    searchfile = fileEnter.get()
+    keywords =  keywordEnter.get()
+    excelfile = outputEnter.get()
+
+    #if html is selected
+    #if ftype.get() == 1:
+        #html function
+        #ReadTextFile()
+
+    #if pdf is selected
+    if ftype.get() == 2:
+       print "PDF"
+       os.system("python pdf2txt.py -o temp.txt " + searchfile + ".pdf") 
+       ReadTextFile("temp.txt", keywords)
+
+    #if txt file is selected
+    if ftype.get() == 3:
+       ReadTextFile(searchfile, keywords)
+
 #Add go button
 GObutton = Button(master,text="Go!", fg="black", command= Go)
 GObutton.grid(row=7,column=6)
+
 
 mainloop()
