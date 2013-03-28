322c3a9a484cb94feebe0de974d595f4e49e3789
Nick Marlo <nwmarlo@gmail.com>
2013-03-28 12:09:26 -0400

    Added words list to excel output

--- excel.py
+++ excel.py
@@ -1,24 +1,47 @@
 import xlwt
 
-filename = excelFile + '.xls'
-if len(searchFile) > 20:
-	article = searchFile[:18] + '...'
+excelfile = 'testfile'
+searchfile = 'Article1'
+
+filename = excelfile + '.xls'
+if len(searchfile) > 20:
+	article = searchfile[:18] + '...'
 else:
-	article = searchFile
+	article = searchfile
+
+new_dict = {}
+
+new_dict['hello'] = 3
+new_dict['goodbye'] = 7
 
 workbook = xlwt.Workbook(encoding = 'ascii')
 worksheet = workbook.add_sheet(article)
+
 font = xlwt.Font()
 font.bold = True
-font.height = 0x00FF
+font.height = 0x010D
 font.underline = True
 style = xlwt.XFStyle()
 style.font = font
-cell0 = 'I'
-worksheet.write_merge(0, 0, 0, 10, searchFile, style)
+
 worksheet.col(0).width = 3333
 worksheet.row(0).height = 400
-worksheet.write(1, 1, 'MADE')
-worksheet.write(2, 1, 'A')
-worksheet.write(3, 1, 'THING')
+
+font2 = xlwt.Font()
+font2.bold = True
+font2.underline = True
+style2 = xlwt.XFStyle()
+style2.font = font2
+
+worksheet.write_merge(0, 0, 0, 10, searchfile, style)
+worksheet.write(1, 1, 'Words', style2)
+worksheet.write(1, 2, 'Count', style2)
+
+index = 2
+
+for w in new_dict:
+     worksheet.write(index, 1, w)
+     worksheet.write(index, 2, new_dict[w])
+     index += 1
+
 workbook.save(filename)
