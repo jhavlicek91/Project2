bf0e44f07a738c634a49b4a0514af4d225481a0c
Nick Marlo <nwmarlo@gmail.com>
2013-03-28 11:26:38 -0400

    excel.py works better with other files

--- excel.py
+++ excel.py
@@ -1,9 +1,24 @@
 import xlwt
 
+filename = excelFile + '.xls'
+if len(searchFile) > 20:
+	article = searchFile[:18] + '...'
+else:
+	article = searchFile
+
 workbook = xlwt.Workbook(encoding = 'ascii')
-worksheet = workbook.add_sheet('My Worksheet')
-worksheet.write(0, 0, label = 'I')
-worksheet.write(1, 0, label = 'MADE')
-worksheet.write(2, 0, label = 'A')
-worksheet.write(3, 0, label = 'THING')
-workbook.save('Test_Workbook.xls')
+worksheet = workbook.add_sheet(article)
+font = xlwt.Font()
+font.bold = True
+font.height = 0x00FF
+font.underline = True
+style = xlwt.XFStyle()
+style.font = font
+cell0 = 'I'
+worksheet.write_merge(0, 0, 0, 10, searchFile, style)
+worksheet.col(0).width = 3333
+worksheet.row(0).height = 400
+worksheet.write(1, 1, 'MADE')
+worksheet.write(2, 1, 'A')
+worksheet.write(3, 1, 'THING')
+workbook.save(filename)
